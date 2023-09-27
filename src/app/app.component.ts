27a50652e4583afa4849fc57a380d89da03251a6
michael-otieno <otieno.oduor.michael>
2023-09-28 00:59:10 +0300

    feat: add total fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import { Component,OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
-import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
-
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 @Component({
@@ -19,52 +33,47 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   invoiceForm!: FormGroup;
-  items!:FormArray;
+  items!: FormArray;
 
   matcher = new MyErrorStateMatcher();
   constructor(private _formBuilder: FormBuilder) {
     this.invoiceForm = this._formBuilder.group({
-        owner_name: ['', Validators.required],
-        owner_address: ['', Validators.required],
-        owner_phone: ['', Validators.required],
-        owner_email: ['', Validators.required,, Validators.email],
-        buyer_name: ['', Validators.required],
-        buyer_address: ['', Validators.required],
-        buyer_phone: ['', Validators.required],
-        buyer_email: ['', Validators.required,, Validators.email],
-        invoice_number: ['', Validators.required],
-        invoice_date: ['', Validators.required],
-        payment_due: ['', Validators.required],
-        items: this._formBuilder.array([]),
-
+      owner_name: ['', Validators.required],
+      owner_address: ['', Validators.required],
+      owner_phone: ['', Validators.required],
+      owner_email: ['', Validators.required, , Validators.email],
+      buyer_name: ['', Validators.required],
+      buyer_address: ['', Validators.required],
+      buyer_phone: ['', Validators.required],
+      buyer_email: ['', Validators.required, , Validators.email],
+      invoice_number: ['', Validators.required],
+      invoice_date: ['', Validators.required],
+      payment_due: ['', Validators.required],
+      items: this._formBuilder.array([]),
     });
   }
 
-
-
   get itemsDetails() {
-    if(this.invoiceForm.get('items')?.invalid){
-
+    if (this.invoiceForm.get('items')?.invalid) {
     }
     return (this.invoiceForm.get('items') as FormArray).controls;
   }
 
-  GetItems():FormGroup{
+  GetItems(): FormGroup {
     return new FormGroup({
       item: new FormControl('', Validators.required),
       quantity: new FormControl('', Validators.required),
       price: new FormControl('', Validators.required),
       amount: new FormControl('', Validators.required),
-    })
+    });
   }
 
-  AddItem(){
+  AddItem() {
     this.items = this.invoiceForm.get('items') as FormArray;
-    this.items.push(this.GetItems())
+    this.items.push(this.GetItems());
   }
-
 
   ngOnInit(): void {
     for (let i = 0; i < 1; i++) {
@@ -158,39 +167,71 @@ export class AppComponent implements OnInit{
 
     // Add form data to the content array
     content.push(
-      { text: `Your Name/Business Name: ${formData.owner_name}`, style: 'owner_name' },
-      { text: `Your Address: ${formData.owner_address}`, style: 'owner_address'},
-      { text: `Your Phone number: ${formData.owner_phone}`,style: 'owner_address' },
-      { text: `Your email: ${formData.owner_email}`,style: 'owner_address' },
+      {
+        text: `Your Name/Business Name: ${formData.owner_name}`,
+        style: 'owner_name',
+      },
+      {
+        text: `Your Address: ${formData.owner_address}`,
+        style: 'owner_address',
+      },
+      {
+        text: `Your Phone number: ${formData.owner_phone}`,
+        style: 'owner_address',
+      },
+      { text: `Your email: ${formData.owner_email}`, style: 'owner_address' },
       { text: 'Bill to:', style: 'sub_header' },
-      { text: `Buyer Name/Business Name: ${formData.buyer_name}`,style: 'owner_address' },
-      { text: `Buyer Address: ${formData.buyer_address}`,style: 'owner_address' },
-      { text: `Buyer Phone number: ${formData.buyer_phone}`,style: 'owner_address' },
-      { text: `Buyer email: ${formData.buyer_email}`,style: 'owner_address' },
-      { text: `Invoice number: ${formData.invoice_number}`,style: 'owner_address' },
-      { text: `Invoice date: ${formData.invoice_date}`,style: 'owner_address' },
-      { text: `Payment due: ${formData.payment_due}`,style: 'owner_address' }
+      {
+        text: `Buyer Name/Business Name: ${formData.buyer_name}`,
+        style: 'owner_address',
+      },
+      {
+        text: `Buyer Address: ${formData.buyer_address}`,
+        style: 'owner_address',
+      },
+      {
+        text: `Buyer Phone number: ${formData.buyer_phone}`,
+        style: 'owner_address',
+      },
+      { text: `Buyer email: ${formData.buyer_email}`, style: 'owner_address' },
+      {
+        text: `Invoice number: ${formData.invoice_number}`,
+        style: 'owner_address',
+      },
+      {
+        text: `Invoice date: ${formData.invoice_date}`,
+        style: 'owner_address',
+      },
+      { text: `Payment due: ${formData.payment_due}`, style: 'owner_address' }
     );
 
     // Add table for items
     const itemsTable = {
       text: '',
-      style: "tableExample",
+      style: 'tableExample',
       table: {
         widths: ['*', '*', '*', '*'],
-        body: [
-          ["Item", "Quantity", "Price per unit", "Amount"],
-        ],
-      }
+        body: [['Item', 'Quantity', 'Price per unit', 'Amount']],
+      },
     };
 
     // Add items data to the table
-    formData.items.forEach((item:any) => {
-      itemsTable.table.body.push([item.item, item.quantity, item.price, item.amount]);
-      itemsTable.table.body.push(['','','Subtotal','$ 0.00'])
+    formData.items.forEach((item: any) => {
+      itemsTable.table.body.push([
+        item.item,
+        item.quantity,
+        item.price,
+        item.amount,
+      ]);
     });
 
-    content.push(itemsTable,);
+    itemsTable.table.body.push(['', '', 'Subtotal', '$0.00']);
+    itemsTable.table.body.push(['', '', 'Tax 0.00%', '$0.00']);
+    itemsTable.table.body.push(['', '', 'Fees/discounts', '$0.00']);
+    itemsTable.table.body.push(['', '', 'TOTAL', '$0.00']);
+
+    // content.push(itemsTable);
+    content.push({...itemsTable });
 
     const docDefinition: any = {
       content: content,
@@ -203,7 +244,7 @@ export class AppComponent implements OnInit{
         sub_header: {
           margin: [0, 10, 0, 4],
           bold: true,
-          fontWeight: 700
+          fontWeight: 700,
         },
         tableExample: {
           margin: [0, 5, 0, 15],
@@ -217,11 +258,8 @@ export class AppComponent implements OnInit{
     pdfDoc.open();
   }
 
-
-
-
   private markFormGroupTouched(formGroup: FormGroup) {
-    Object.values(formGroup.controls).forEach(control => {
+    Object.values(formGroup.controls).forEach((control) => {
       if (control instanceof FormGroup) {
         this.markFormGroupTouched(control);
       } else {
@@ -229,5 +267,4 @@ export class AppComponent implements OnInit{
       }
     });
   }
-
 }
